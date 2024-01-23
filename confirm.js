1187b9886d71af9419316bfd700ed409516b0ee4
Nathan Fernandez <nathanfernandez@nathans-mbp.mynetworksettings.com>
2024-01-23 11:51:43 -0500

    Confirm Password function working.

--- confirm.js
+++ confirm.js
@@ -3,6 +3,8 @@ function getDetails() {
   let cpw = document.getElementById("confirm").value;
   if (password != cpw) {
     alert("Enter the same password in 'Confirm Password'");
+  } else {
+    alert("Account successfully created!");
   }
   console.log(password);
 }
